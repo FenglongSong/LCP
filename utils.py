36305d873161722fec1e85268e51352d7a4079b4
Longlongago <40928472+FenglongSong@users.noreply.github.com>
2024-01-03 23:50:17 +0100

    Add Hyperplane Representation of a (inequality constrained) polyhedron. Add a function to remove redundant inequality.

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import casadi as ca
 
 def is_lexico_positive(matrix: np.ndarray, zero_tol=1e-10) -> bool:
       """ Tell if a matrix or vector is lexico positive.
@@ -68,4 +69,70 @@ def lexico_argmin(matrix: np.array, zero_tol=1e-10) -> int | list[int]:
 
       return argmin
 
+class HyperplaneRepresentation:
+      """
+      Define a H-reprenestation of a polyhedron.
+      """
       
+      def __init__(self, A: np.ndarray, b: np.ndarray) -> None:
+            if A.shape[0] != b.shape[0]:
+                  raise ValueError("HRepresentation: Dimension inconsistency between A and b!")
+
+            self.A = A
+            self.b = b
+            self.m = A.shape[0] # number of affine constraints
+            self.n = A.shape[1] # number of dimension
+
+      def remove_redundant_constraints(self) -> None:
+            """ Remove all the redundant constraints.
+            
+            Reference:
+                  Section 2.20 of https://people.inf.ethz.ch/fukudak/Doc_pub/polyfaq220115c.pdf
+            
+            We want to test whether the subsystem of first m-1 inequalities Cx \leq d implies the last inequality s x \leq t. 
+            If so, the inequality s x \leq t is redundant and can be removed from the system. A linear programming (LP) 
+            formulation of this checking is rather straightforward:
+
+            f^* = max s^T x
+                  s.t C x \leq d,
+                      s x \leq t + 1
+            Then the inequality s x \leq t is redundant if and only if the optimal value f^* is less than or equal to t.
+            """
+            
+            i = 0
+            while True:
+                  s = self.A[[i], :]
+                  t = self.b[i]
+
+                  C = np.vstack((self.A[:i, :], self.A[i+1:, :]))
+                  d = np.concatenate((self.b[:i], self.b[i+1:]))
+
+                  opti = ca.Opti()
+                  x = opti.variable(self.n)
+                  opti.subject_to(C @ x <= d)
+                  opti.subject_to(s @ x <= t+1)
+                  opti.minimize(-s@x)
+
+                  opts = {'ipopt.print_level':0, 'print_time':0}
+                  opti.solver('ipopt', opts)
+
+                  sol = opti.solve()
+                  if not sol.stats()['success']:
+                        raise RuntimeError("Ipopt failure.")
+                  
+                  x_opt = sol.value(x)
+                  f_opt = s @ x_opt
+                  if f_opt <= t: # redundant
+                        print("Found a redundant constraint ", s)
+                        self.A = C
+                        self.b = d
+                        self.m -= 1
+                  else:
+                        i += 1
+
+                  if i == self.m:
+                        break
+
+                  # A very naive way to avoid dead cycle
+                  if i >= 5000:
+                        raise RuntimeError("Dead cycle.")
